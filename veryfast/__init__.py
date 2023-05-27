de9bbd986208fffb8ab6cb4764f9bf18f64f92e9
Eugen Ciur <eugen@papermerge.com>
2023-05-27 07:28:25 +0200

    exploring loggers

--- veryfast/__init__.py
+++ veryfast/__init__.py
@@ -8,6 +8,13 @@ logger = logging.getLogger(__name__)
 
 
 @app.get("/")
-async def root():
+async def root_endpoint():
     logger.info("root")
     return {"message": "Hello World"}
+
+
+@app.get("/version")
+async def get_version():
+    logger.info("getting the app version")
+    logger.debug("This is debug level info")
+    return {"version": "1.0.2"}
